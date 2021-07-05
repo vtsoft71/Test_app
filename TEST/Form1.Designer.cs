5bf9029a9bb624aec7630f51d06c76b9e6a678d0
Hung <hungse@gmail.com>
2021-07-05 14:09:02 +0700

    Add project files.

---
+++ TEST/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.download_file = new System.Windows.Forms.Button();
+            this.unrar_file = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(882, 350);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // download_file
+            // 
+            this.download_file.Location = new System.Drawing.Point(49, 428);
+            this.download_file.Name = "download_file";
+            this.download_file.Size = new System.Drawing.Size(176, 38);
+            this.download_file.TabIndex = 1;
+            this.download_file.Text = "Down load file";
+            this.download_file.UseVisualStyleBackColor = true;
+            this.download_file.Click += new System.EventHandler(this.download_file_Click);
+            // 
+            // unrar_file
+            // 
+            this.unrar_file.Location = new System.Drawing.Point(303, 428);
+            this.unrar_file.Name = "unrar_file";
+            this.unrar_file.Size = new System.Drawing.Size(164, 38);
+            this.unrar_file.TabIndex = 2;
+            this.unrar_file.Text = "Unrar file";
+            this.unrar_file.UseVisualStyleBackColor = true;
+            this.unrar_file.Click += new System.EventHandler(this.unrar_file_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 628);
+            this.Controls.Add(this.unrar_file);
+            this.Controls.Add(this.download_file);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button download_file;
+        private System.Windows.Forms.Button unrar_file;
+    }
+}
+
---
+++ TEST/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEST
+{
+    public partial class Form1 : Form
+    {
+        string sqlite_ConnectionString = "URI=file:" + @"C:\Users\Admin\Documents\Visual Studio 2015\Projects\VTNET\VTNET\vtdat.db";
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.DataSource = SelectCommand("Select * from listcomp");
+        }
+
+        private void sdfsd()
+        {
+
+        }
+        public DataTable SelectCommand(string sql_cmd)
+        {
+            SQLiteConnection con = new SQLiteConnection(sqlite_ConnectionString);
+            con.Open();
+            DataTable tbl_select = new DataTable();
+            using (SQLiteDataAdapter da = new SQLiteDataAdapter(sql_cmd, con))
+            {
+                da.Fill(tbl_select);
+            }
+            con.Close();
+            con.Dispose();
+            return tbl_select;
+        }
+        public void RunSqlCommand(string sql_cmd)
+        {
+            try
+            {
+                SQLiteConnection con = new SQLiteConnection(sqlite_ConnectionString);
+                SQLiteCommand command = new SQLiteCommand(sql_cmd.Replace("N'", "'"), con);
+                con.Open();
+                command.ExecuteNonQuery();
+                command.Dispose();
+                con.Close();
+                con.Dispose();
+            }
+            catch
+            {
+                MessageBox.Show("Sqlite: " + sql_cmd, "Sqlite error command", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void download_file_Click(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("http://103.226.249.246:8088/VFP6.rar", @"C:\Users\Admin\Desktop\test_setup\VFP6.rar");
+            }
+            MessageBox.Show("downloaded!");
+        }
+
+        private void unrar_file_Click(object sender, EventArgs e)
+        {
+            const string fileName = @"C:\Users\Admin\Desktop\test_setup\VFP6.rar";
+            string destinationFolder = fileName.Remove(fileName.LastIndexOf('.'));
+            System.IO.Directory.CreateDirectory(destinationFolder);
+
+            // Install-Package nunrar
+            NUnrar.Archive.RarArchive.WriteToDirectory(fileName, destinationFolder);
+            MessageBox.Show("Finish unrar!");
+        }
+    }
+}
